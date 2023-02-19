0b33ba90af9fc0595761e89e76893b4cff0e608e
Diego Estefano <81423690+DiegoEstefano@users.noreply.github.com>
2023-02-19 13:38:00 -0300

    Componentização

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 import DevContainer from "./components/DevContainer";
 import NavBar from "./components/NavBar";
 import SocialMedia from "./components/SocialMedia";
@@ -14,18 +8,15 @@ import PortiFolio from "./components/PortiFolio";
 
 function App() {
   return (
-      <div className="bg-white px-10 md:px-40 lg:px-60">
-        <NavBar />
-        <DevContainer />
-        <SocialMedia />
-        <MyStacks />
-        <WantToLearn/>
-        <PortiFolio/>
-        
-        {/*************************** Portfolio ***************************/}
-         
-      <Footer/>
-      </div>
+    <div className="bg-white px-10 md:px-40 lg:px-60">
+      <NavBar />
+      <DevContainer />
+      <SocialMedia />
+      <MyStacks />
+      <WantToLearn />
+      <PortiFolio />
+      <Footer />
+    </div>
   );
 }
 
